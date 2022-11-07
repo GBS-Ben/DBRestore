2cffa07e4d826474cb8b0275e250da09df16ba97
GBS-Ben <ben@markful.com>
2022-11-07 14:47:27 -0800

    Post build files added

---
+++ Sql07Restore/Sql07Restore/obj/Debug/postdeploy.sql
@@ -0,0 +1,50 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+select @@servername
+SELECT SERVERPROPERTY('ServerName')
+
+DECLARE @TrueDate VARCHAR(Max)
+SET @TrueDate = (select cast(right('0' + cast (datepart(month,getdate()) as varchar),2) + right('0' + cast (datepart(day,getdate()) as varchar),2) + cast(datepart(year,getdate()) as varchar) as numeric(10,0)))
+DECLARE @DiskFile VARCHAR(MAX)
+SET @DiskFile = CONCAT(N'\\summerhall\DEVOPS\Summerhall-SQL01\azureDBs\aznopcommerce_', @TrueDate,'.bak')
+use [nopcommerce]
+USE [master]
+if @@servername='DEV-SQL07\NOP'
+begin
+ALTER DATABASE [nopcommerce] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+RESTORE DATABASE [nopcommerce]
+FROM DISK= @DiskFile
+WITH FILE = 1
+, MOVE N'nopcommerce' TO N'F:\NOP\DATA\nopcommerce.mdf'
+, MOVE N'nopcommerce_log' TO N'E:\NOP\LOG\nopcommerce_log.LDF'
+, NOUNLOAD, REPLACE
+, STATS = 5
+ALTER DATABASE [nopcommerce] SET MULTI_USER ALTER DATABASE [nopcommerce] SET RECOVERY SIMPLE WITH NO_WAIT
+--check that sqlsvc_intranet has read permissions on db
+USE [nopcommerce]
+--GO
+CREATE USER [sqlsvc_intranetdev] FOR LOGIN [sqlsvc_intranetdev]
+--GO
+USE [nopcommerce]
+--GO
+ALTER ROLE [db_datareader] ADD MEMBER [sqlsvc_intranetdev]
+--GO
+end
+else
+begin
+print 'YOU ALMOST RESTORED ON A NON-QA SERVER!!!!!'
+return
+end
+GO
+
+
+GO
